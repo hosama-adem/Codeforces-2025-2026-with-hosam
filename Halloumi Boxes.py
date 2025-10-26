3992a6af2c61cf64c66f8a20bc06c82deae6914c
hos <hosamaadem8@gmail.com>
2025-10-26 14:04:29 -0700

    Create  Halloumi Boxes.py

---
+++ Halloumi Boxes.py
@@ -0,0 +1,9 @@
+#hos
+t=int(input())
+for _ in range (t):
+    n,k=map(int,input().split())
+    a=list(map(int,input().split()))
+    if k==1 and  sorted(a)!=a:
+        print("NO")
+    else:
+        print("YES")
